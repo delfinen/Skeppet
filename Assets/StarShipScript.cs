00c5315d484d10749dcab053db1576b8c68d3542
LEARNET\daniel.aleksovski <daniel.aleksovski@elev.ga.lbs.se>
2018-10-09 12:05:39 +0200

    c uppgifterna

--- Assets/StarShipScript.cs
+++ Assets/StarShipScript.cs
@@ -12,11 +12,14 @@ public class StarShipScript : MonoBehaviour
     public float timestart;
 
 
+
+
     // Use this for initialization
     void Start()
     {
         timestart = 0;
         timer = timestart;
+        rotationspeed = 200;
     }
 
     // Update is called once per frame
@@ -24,16 +27,23 @@ public class StarShipScript : MonoBehaviour
     {     // När man klickar på tangenten A så kommer något att hända
         if (Input.GetKey(KeyCode.A))
         {    // första kommandot gör så att när knappen a trycks ner så svänger den åt höger och det andra i mosvingarna gör så att när man trycker a blir skeppet grönt
+
             transform.Rotate(0f, 0f, rotationspeed * Time.deltaTime);
             rend.color = new Color(0f, 1f, 0f);
+            rotationspeed = 100;
+            // gör så den svänge hälften av vad rotationspeed = med 
+
         }
 
 
         // När man klickar påc tangenten D så kommer något att hända
         if (Input.GetKey(KeyCode.D))
         { // första kommandot gör så att när knappen a trycks ner så svänger den åt vänster och det andra i mosvingarna gör så att när man trycker a blir skeppet blå
+
             transform.Rotate(0f, 0f, -rotationspeed * Time.deltaTime);
             rend.color = new Color(0f, 0f, 1f);
+            rotationspeed = 200;
+            // gär så skeppet svänger i farten 200 hundra när man klickar d
         }
         // När man klickar påc tangenten s så kommer något att hända
 
@@ -45,9 +55,23 @@ public class StarShipScript : MonoBehaviour
 
         // ger skeppet fart så den åker framåt
         transform.Translate(5f * Time.deltaTime, 0, 0, Space.Self);
-        // den gör så att timern ökar med 1 sec och det under printar ut timer när det händer
-        timer = timer + 1 * Time.deltaTime;
-        print("Timer" + timer);
+
+        // gör så timern printar värdet varje sekund
+        timer += Time.deltaTime;
+        // printar varje sekund och gör så det står timer
+        Debug.Log(string.Format("{0}", timer));
+
+        // när man klickar space händer något
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // gör så att det bli en random färg mellan röd grön och blå
+            rend.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+
+
+
+        }
+
+
 
 
 
